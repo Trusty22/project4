9cdb266044868caa08652777edc2962362b807a3
jr-pedersen <129087875+jr-pedersen@users.noreply.github.com>
2023-12-12 22:09:49 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
+using namespace std;
+#include "movie.h"
+#include "customer.h"
+#include "inventory.h"
+#include "store.h"
+#include <sstream>
+#include <iostream>
+#include <fstream>
 
+int main(){
+    store store0;
+}
